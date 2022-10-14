ea10b3032c9dc0ecb544c0e97ce349ed6df6548c
Benjamin Eppinger <bxe5056@gmail.com>
2022-10-14 19:52:45 -0400

    WIP: Initial File Structure

---
+++ src/MenuBar/MenuBar.jsx
@@ -0,0 +1,12 @@
+import './MenuBar.css'
+
+function MenuBar() {
+    return (
+        <div className="MenuBar">
+            S
+        </div>
+    )
+}
+
+
+export default MenuBar
---
+++ src/Viewport/Viewport.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './Viewport.css'
+
+function Viewport() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="ViewPort">
+      <h1>Vite + React</h1>
+      <div className="left">
+
+      </div>
+        <div className="left">
+
+        </div>
+        <div className="left">
+    
+        </div>
+    </div>
+  )
+}
+
+export default Viewport

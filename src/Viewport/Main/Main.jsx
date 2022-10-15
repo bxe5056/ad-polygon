93191da1848335a5e0c72e9fb8bda43097d0a52d
Benjamin Eppinger <bxe5056@gmail.com>
2022-10-15 01:14:10 -0400

    Enable Union Handling

--- src/Viewport/Main/Main.jsx
+++ src/Viewport/Main/Main.jsx
@@ -1,44 +1,53 @@
-import React, { useState } from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import './Main.css'
-import leaflet from 'leaflet'
 import 'leaflet/dist/leaflet.css'
-import { lineString, bbox, bboxPolygon } from "@turf/turf";
-import {MapContainer, Marker, Polygon, Popup, TileLayer} from "react-leaflet";
+import { lineString, bbox } from "@turf/turf";
+import {MapContainer, Marker, Polygon, Popup, TileLayer, useMapEvent} from "react-leaflet";
 
 
 function Main(props) {
+
     // eslint-disable-next-line react/prop-types
-    let geoJSON = [...props.geojson.features];
+    let [geoJSON, setgeoJSON] = useState([...props.geojson.features]);
+    // eslint-disable-next-line react/prop-types
+    useEffect(() => { setgeoJSON([...props.geojson.features]); }, [props.geojson.features]);
+
+    let getBounds = () => {
+        let allCoords = [...geoJSON.map((polygon) => {
+            return polygon.geometry.coordinates
+        })].flat(2)
+
+        let line = lineString(allCoords);
+        let bboxArray = bbox(line);
+
+        let corner1 = [bboxArray[0], bboxArray[1]];
+        let corner2 = [bboxArray[2], bboxArray[3]];
+
+        return [corner1, corner2]
+    }
+
+    const [currentBound, updateCurrentBound] = useState(getBounds)
 
 
-    // let leafletObject = () => leaflet.map('map').setView([51.505, -0.09], 13);
-    let allCoords = [...geoJSON.map((polygon) => {
-        return polygon.geometry.coordinates
-    })].flat(2)
-    let line = lineString(allCoords);
-    let bboxArray = bbox(line);
-    const corner1 = [bboxArray[0], bboxArray[1]];
-    const corner2 = [bboxArray[2], bboxArray[3]];
-    let bounds = [corner1, corner2]
-    // let bboxPolygons = bboxPolygon(bboxs);
+    const didMountRef = useRef(false)
 
-    const position = [51.505, -0.09]
+
 
     let getItems = () => {
         let out = geoJSON.map((feature, index) => {
-            console.log("COORDINATES: ", feature.geometry.coordinates[0])
+            // console.log("COORDINATES: ", feature.geometry.coordinates[0])
             return (
                 <Polygon pathOptions={{ color: 'purple' }} positions={feature.geometry.coordinates[0]} key={index}/>
             )
         })
-        console.log("AllCoords: ", bounds)
+        console.log("AllCoords: ", JSON.stringify(currentBound))
         return out;
     }
 
     return (
         <div className="Viewer">
             {/*{JSON.stringify(geoJSON)}*/}
-            <MapContainer className={"MapContainer"} bounds={bounds} scrollWheelZoom={false}>
+            <MapContainer className={"MapContainer"} bounds={currentBound} scrollWheelZoom={false} geoJSON={geoJSON}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://www.pngmagic.com/product_images/solid-dark-grey-background.jpg"
--- src/Viewport/Viewport.jsx
+++ src/Viewport/Viewport.jsx
@@ -1,51 +1,124 @@
 import {useEffect, useState} from 'react'
 import './Viewport.css'
-import Right from "./Right/Right";
-import Left from "./Left/Left";
 import Main from "./Main/Main";
 
 import GeoJSON1 from '../GeoJSON/SE_State_Management_Polygons_1.json'
 import GeoJSON2 from '../GeoJSON/SE_State_Management_Polygons_2.json'
+import {union} from "@turf/turf";
 
-let PolygonObjects = [
-    {...GeoJSON1},
-    {...GeoJSON2}
-]
+
 
 function Viewport() {
-    const [currentItem, setCurrentItem] = useState(0)
+    let [polygonObjects, setPolygonObjects] = useState([
+        {...GeoJSON1},
+        {...GeoJSON2}
+    ])
 
-    function handleButton(e, index) {
+
+
+    const [currentItem, setCurrentItem] = useState(0)
+    const [currentObject, setCurrentObject] = useState(polygonObjects[0])
+    const [firstSelection, setFirstSelection] = useState([])
+    const [secondSelection, setSecondSelection] = useState([])
+
+    function handleFeatureCollectionButton(e, index) {
         e.preventDefault();
         setCurrentItem(index)
+        setCurrentObject(polygonObjects[index])
     }
 
-    useEffect(() => {
-        console.log('Selected Item: ', currentItem);
-        console.log('Selected Poly: ', JSON.stringify(PolygonObjects[currentItem]));
-    }, [currentItem]);
+    function handlePolygonButton(e, index, pIndex) {
+        e.preventDefault();
+        if(firstSelection.length === 0) {
+            setFirstSelection([index, pIndex])
+            setCurrentItem(index)
+            setCurrentObject(polygonObjects[index])
+        }
+        else if(secondSelection.length === 0 && index === firstSelection[0]) {
+            setSecondSelection([index, pIndex])
+        } else {
+            setFirstSelection([])
+            setSecondSelection([])
+        }
+        console.log("Item: ", JSON.stringify(polygonObjects[index].features[pIndex]))
+    }
+
+    function handleUnion(e) {
+        e.preventDefault();
+        let currFeatureCollection = polygonObjects[firstSelection[0]]
+        let theUnion = union(
+            currFeatureCollection.features[firstSelection[1]],
+            currFeatureCollection.features[secondSelection[1]])
+        console.log("UNION: ", JSON.stringify(theUnion))
+
+
+
+        const removeValFromIndex = [firstSelection[1], secondSelection[1]];
+
+        const indexSet = new Set(removeValFromIndex);
+
+        const arrayWithValuesRemoved = currFeatureCollection.features.filter((value, i) => !indexSet.has(i));
+
+        currFeatureCollection = {
+            ...currFeatureCollection,
+            features: [...arrayWithValuesRemoved, {
+                ...theUnion
+            }]
+        }
+
+        let t = [...polygonObjects];
+        t[firstSelection[0]] = currFeatureCollection;
+        setPolygonObjects(t)
+
+        setCurrentObject(currFeatureCollection)
+
+
+        console.log(JSON.stringify(polygonObjects[firstSelection[0]]))
+        console.log(JSON.stringify(currFeatureCollection))
+
+    }
 
     let listOfPolygons = () => {
-        return PolygonObjects.map( (element, index) => {
-            return <button className={"list-polygon-item"} key={index} onClick={e => handleButton(e, index)}>Object {index + 1}</button>
+        return polygonObjects.map( (element, index) => {
+            return <div key={index}>
+                <button className={"list-feature-collection"} key={`${index}-main`} onClick={e => handleFeatureCollectionButton(e, index)}>Collection {index + 1}</button>
+                {
+                    element.features.map((polygon, pIndex) => {
+                        return <div key={pIndex}>
+                            <button className={"list-polygon-item"} key={`${pIndex}-main`} onClick={e => handlePolygonButton(e, index, pIndex)} >Polygon {pIndex + 1}</button>
+                        </div>
+                    })
+
+                }
+            </div>
+
         } )
     }
 
     return (
         <div className="ViewPort">
-            <h1>Vite + React</h1>
+            <h1>Polygon Calculation Machine</h1>
             <div className="Left">
-                <Left/>
                 <div className={"Selector"}>
                     {listOfPolygons()}
                 </div>
             </div>
             <div className="Main">
-                <Main geojson={PolygonObjects[currentItem]}/>
+                <Main geojson={currentObject}/>
             </div>
-            {/*<div className="Right">*/}
-            {/*    <Right/>*/}
-            {/*</div>*/}
+            <div className="Right">
+                <h2 className={"Selector2"}>
+                    {firstSelection.length ? `In Collection: ${(firstSelection[0] + 1)}` : ''}
+                    <br/>
+                    {firstSelection.length ? `You have selected: ` : ''}
+                    <br/>
+                    {firstSelection.length ? `Polygon: ${(firstSelection[1] + 1)} ` : ''}
+                    <br/>
+                    {secondSelection.length ? `Polygon: ${(secondSelection[1] + 1)} ` : ''}
+
+                    <button onClick={handleUnion}/>
+                </h2>
+            </div>
         </div>
     )
 }
